b6aaeb8d0d8e91ed7aabf93f2cdc4fcc6788b119
Dalena Pham <16dpham@gmail.com>
2025-05-20 11:54:37 -0700

    Added class04 content

---
+++ class04/class04table.R
@@ -0,0 +1,13 @@
+#' ---
+#' title: "Intro to R"
+#' author: "Dalena Pham (PID:A17327787)"
+#' date: "April 10th, 2025"
+#' ---
+
+#My first R script
+x<-1:50
+plot(x,sin(x))
+
+plot(x,sin(x),typ="l",col="purple",lwd=3,xlab="Silly x axis",
+ylab="Sensible y axis")
+
